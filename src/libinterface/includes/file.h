857ff1e4ee44119df1800c5c09d88ca83be5d6a1
Germano Barcelos <gegenbarcelos@gmail.com>
2019-11-28 22:19:12 -0300

    adding .h in file.h

--- src/libinterface/includes/file.h
+++ src/libinterface/includes/file.h
@@ -10,4 +10,14 @@
 #include "../../liblinked/includes/linkedtext.h"
 #include "../../liblinked/includes/linkedbook.h"
 
+#include "./includes/file.h"
+
+void fileReadVectorWord(char *file, Word *word, int length, int *position);
+void fileReadVectorText(char *file, Text *text, int wordLength, int textLength, int *position);
+void fileReadVectorBook(char *file, Book *book, int wordLength, int textLength, int bookLength, int *position);
+
+void fileReadLinkedWord(char *file, LinkedWord *word, int length, int *position);
+void fileReadLinkedText(char *file, LinkedText *text, int wordLength, int textLength, int *position);
+void fileReadLinkedBook(char *file, LinkedBook *book, int wordLength, int textLength, int bookLength, int *position);
+
 #endif
