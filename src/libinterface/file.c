7aa78a80a00b045f7b9515f8a996ea15b7bd5042
Germano Barcelos <gegenbarcelos@gmail.com>
2019-11-28 22:16:03 -0300

    Reading in sequence

--- src/libinterface/file.c
+++ src/libinterface/file.c
@@ -18,49 +18,53 @@ void fileReadVectorWord(char *file, Word *word, int length, int *position) {
   position = ftell(ptr);
 }
 
-void fileReadVectorText(char *file, Text *text, int wordLength, int textLength) {
+void fileReadVectorText(char *file, Text *text, int wordLength, int textLength, int *position) {
   int i;
   for (i = 0; i < textLength; i++) {
     Word word;
-    fileReadVectorWord(file, &word, wordLength);
+    fileReadVectorWord(file, &word, wordLength, position);
     textInsertWord(text, word);
   }
 }
-void fileReadVectorBook(char *file, Book *book, int wordLength, int textLength, int bookLength) {
+void fileReadVectorBook(char *file, Book *book, int wordLength, int textLength, int bookLength, int *position) {
   int i;
   for (i = 0; i < bookLength; i++) {
     Text text;
-    fileReadVectorText(file, &text, wordLength, textLength);
+    fileReadVectorText(file, &text, wordLength, textLength, position);
     bookInsertText(book, text);
   }
 }
 
-void fileReadLinkedWord(char *file, LinkedWord *word, int length) {
+void fileReadLinkedWord(char *file, LinkedWord *word, int length, int *position) {
   FILE *ptr;
 
   ptr = fopen(file, "r+");
 
   int counter = length;
 
+  fseek (ptr, position, SEEK_SET);
+
   while (!feof(ptr) && --counter) {
     char c;
     fread(&c, 1, 1, ptr);
     LinkedWordInsert(word, c);
   }
+
+  position = ftell(ptr);
 }
-void fileReadLinkedText(char *file, LinkedText *text, int wordLength, int textLength) {
+void fileReadLinkedText(char *file, LinkedText *text, int wordLength, int textLength, int *position) {
   int i;
   for (i = 0; i < textLength; i++) {
     LinkedWord word;
-    fileReadLinkedWord(file, &word, wordLength);
+    fileReadLinkedWord(file, &word, wordLength, position);
     LinkedTextInsert(text, word);
   }
 }
-void fileReadLinkedBook(char *file, LinkedBook *book, int wordLength, int textLength, int bookLength) {
+void fileReadLinkedBook(char *file, LinkedBook *book, int wordLength, int textLength, int bookLength, int *position) {
   int i;
   for (i = 0; i < bookLength; i++) {
     LinkedText text;
-    fileReadLinkedText(file, &text, wordLength, textLength);
+    fileReadLinkedText(file, &text, wordLength, textLength, position);
     LinkedBookInsert(book, text);
   }
 }
